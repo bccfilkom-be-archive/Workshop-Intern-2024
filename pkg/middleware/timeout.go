f7d3917412b4ae519446bca9cfc70e525a7d7bcd
reyhanmichiels <michiels.reyhan@gmail.com>
2024-02-29 20:06:15 +0700

    create time out middleware

---
+++ pkg/middleware/timeout.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/Ndraaa15/workshop-bcc/pkg/response"
+	"github.com/gin-contrib/timeout"
+	"github.com/gin-gonic/gin"
+)
+
+func Timeout() gin.HandlerFunc {
+	timeLimit, _ := strconv.Atoi(os.Getenv("TIME_OUT_LIMIT"))
+
+	return timeout.New(
+		timeout.WithTimeout(time.Duration(timeLimit)*time.Second),
+		timeout.WithHandler(func(c *gin.Context) {
+			c.Next()
+		}),
+		timeout.WithResponse(timeoutResponse),
+	)
+}
+
+func timeoutResponse(c *gin.Context) {
+	response.Error(c, http.StatusRequestTimeout, "the request take to much time", errors.New(""))
+}
